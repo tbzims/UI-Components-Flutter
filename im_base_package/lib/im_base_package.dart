47ae8161dbfadf1a8cbbdc3e8e723e956e4c43d5
Jeremy <xuejinming@snto.com>
2025-09-25 15:15:46 +0800

    修改项目结构

---
+++ im_base_package/lib/im_base_package.dart
@@ -0,0 +1,16 @@
+library im_base_package;
+
+/// 主题样式
+export 'src/theme/im_theme.dart';
+export 'src/theme/im_colors.dart';
+
+/// 多语言
+export 'generated/intl/app_localizations.dart';
+
+/// 按钮
+export 'src/components/button/im_button.dart';
+export 'src/components/button/im_button_style.dart';
+export 'src/components/button/im_button_group.dart';
+
+/// 加载样式
+export 'src/components/loading/im_loading.dart';
